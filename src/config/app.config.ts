ff7a5b9534d813fa9912c49aaa6eb910dc300c0f
Arya Shahali <ars@saxopayments.com>
2020-12-28 18:11:13 +0100

    changed to port 5000

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  port: process.env.PORT ?? 8080,
+  port: process.env.PORT ?? 5000,
   host: process.env.HOST ?? 'localhost',
   DBHost: process.env.DB_DOMAIN,
 }));
